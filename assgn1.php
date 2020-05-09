20502b81440a1eb7a76a65bf281647ce1af3603e
pratyush-12 <36147813+pratyush-12@users.noreply.github.com>
2020-05-09 12:03:21 +0530

    Ajax Assignment

---
+++ assgn1.php
@@ -0,0 +1,13 @@
+<html>
+<head>  
+ <script src="js/jquery-3.5.1.js"></script>
+ <script src="js/assgn1.js"></script>
+ <link rel="stylesheet" type="text/css" href="css/assgn1.css">
+</head>
+
+<body>
+<h3 align="center"> Details</h3>
+<div class="detail"></div>
+
+</body>
+</html>
