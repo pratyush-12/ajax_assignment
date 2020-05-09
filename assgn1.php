8aabd77c023edbc8f89d679a4fbb099aee0d4603
pratyush-12 <36147813+pratyush-12@users.noreply.github.com>
2020-05-09 12:57:24 +0530

    Proper formatted

--- assgn1.php
+++ assgn1.php
@@ -1,13 +1,11 @@
-<html>
+<!DOCTYPE html>
 <head>  
- <script src="js/jquery-3.5.1.js"></script>
- <script src="js/assgn1.js"></script>
- <link rel="stylesheet" type="text/css" href="css/assgn1.css">
+	<script src="js/jquery-3.5.1.js"></script>
+	<script src="js/assgn1.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/assgn1.css">
 </head>
-
 <body>
-<h3 align="center"> Details</h3>
-<div class="detail"></div>
-
+	<h3 align="center"> Details</h3>
+	<div class="detail"></div>
 </body>
 </html>
--- js/assgn1.js
+++ js/assgn1.js
@@ -4,16 +4,15 @@ $(document).ready(function doAjax() {
         url: "display.php",             
         dataType: "json",              
         success: function(response){    
-              newitem = $("<table border='2' align='center' cellpadding='10'> <tr> <th>Name</th> <th>Email</th> <th>Message</th> <th>Date</th> </tr>");
-              newitem = "<table> <tr> <th>Name</th> <th>Email</th> <th>Message</th> <th>Date</th> </tr>";
-              $.each( response, function( key, value ) {
-                newitem = newitem + " <tr> <td>" +value.name + "</td> <td>"+value.email +"</td> <td>"+value.message +"</td> <td>"+value.date +"</td> </tr>";               
-              });
-               newitem = newitem + "</table>";
-              $(".detail").replaceWith(newitem);
-              setInterval(function(){
-                  doAjax() 
-                }, 5000);
+            newitem = $("<table border='2' align='center' cellpadding='10'> <tr> <th>Name</th> <th>Email</th> <th>Message</th> <th>Date</th> </tr>");
+            newitem = "<table> <tr> <th>Name</th> <th>Email</th> <th>Message</th> <th>Date</th> </tr>";
+            $.each( response, function( key, value ) {
+              newitem = newitem + " <tr> <td>" +value.name + "</td> <td>"+value.email +"</td> <td>"+value.message +"</td> <td>"+value.date +"</td> </tr>";               
+            });
+            newitem = newitem + "</table>";
+            $(".detail").replaceWith(newitem);
+            setInterval(function() { 
+              doAjax() }, 5000);
         }
     });
 });
