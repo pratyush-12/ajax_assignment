5b19abbbb934ce096b0317203df802bea695565f
pratyush-12 <36147813+pratyush-12@users.noreply.github.com>
2020-05-15 11:51:01 +0530

    Pagination edited with ajax

--- display.php
+++ display.php
@@ -2,9 +2,9 @@
 $con=mysqli_connect("localhost","team_magento","team_magento@123","team_magento_new_joinee_pratyush"); 
 $result = array();
 if ($_REQUEST['column_name'] != "" && $_REQUEST['sort_by'] != "") {
-	$query=mysqli_query($con,"select * from student ORDER BY ".$_REQUEST['column_name']." ".$_REQUEST['sort_by']." LIMIT 5");
+	$query=mysqli_query($con,"select * from student ORDER BY ".$_REQUEST['column_name']." ".$_REQUEST['sort_by']." LIMIT 10");
 }else{
-	$query=mysqli_query($con,"select * from student ORDER BY id ASC LIMIT 5");
+	$query=mysqli_query($con,"select * from student ORDER BY id ASC LIMIT 10");
 }
 
 while($row = $query->fetch_object()) {
--- js/assgn1.js
+++ js/assgn1.js
@@ -1,39 +1,24 @@
 $(document).ready(function() { 
-  attr = "";
-  col_name = "";
-  item = "";
-  table = "";
-  maxRows = 3;
-  doAjax("","");
+  
+  attr = "";  col_name = "";  pagenum = 1;  maxRows = 3;  pageNum = 1;  totalRows = 10;  result_array = [];
+  doAjax("","",pageNum);
     
-  function doPagination() {
-    table = "#mytable";
+  function doPagination(clickedPage) {
+    maxRows = 3;
+    totalRows = 10;
     $('.pagination').html('');
-    var trnum = 0;
-    var totalRows = $(table +' tbody tr').length;
-    if (totalRows > maxRows) {
-      var pagenum = Math.ceil(totalRows / maxRows);
-      for(var i=1; i<=pagenum;){
-        $('.pagination').append('<li class="page-item page-link" data-page="'+i+'">'+ i++ +'<span class="sr-only">(current)</span> </li>').show();
-      }
-    }  
-    $('.pagination li:first-child').addClass('active');
-    $('.pagination li').click(function() {
-      var pageNum = $(this).attr('data-page');
-      var trIndex = 0; 
+    pagenum = Math.ceil(totalRows / maxRows);
+    for(var i=1; i<=pagenum;){
+      $('.pagination').append('<li class="page-item page-link" data-page="'+i+'">'+ i++ +'<span class="sr-only">(current)</span> </li>').show();
+    } 
+    if(clickedPage){
       $('.pagination li').removeClass('active');
-      $(this).addClass('active');
-      $(table+' tr:gt(0)').each(function() {
-        trIndex++;
-        if(trIndex > (maxRows*pageNum) || trIndex <= ((maxRows*pageNum) - maxRows))
-        {
-          $(this).hide();
-        }else{
-          $(this).show();
-        }
-      });
+      $('[data-page="'+clickedPage+'"]').addClass('active');
+    }
+    $('.pagination li').click(function() {
+      pageNum = $(this).attr('data-page');
+      doAjax("","",pageNum);
     });
-    $('.pagination li:first-child').trigger("click");
   }
 
   $('th').click(function(){
@@ -45,10 +30,10 @@ $(document).ready(function() {
     }else{
       order = "ASC";
     }
-    doAjax(attr,order);
+    doAjax(attr,order,pageNum);
   });
 
-  function doAjax(attr,order) {
+  function doAjax(attr,order,pageNo) {
     if (attr != "" && order != "") {
       col_name = attr;
       order_by = order;
@@ -57,26 +42,41 @@ $(document).ready(function() {
       order_by = "";
     }
     var data = {column_name:col_name,sort_by:order_by};
-    callAjax(data);
-    setInterval(function() { doAjax(col_name,order_by); }, 10000);
+    callAjax(data,pageNo);
+    // setInterval(function() { doAjax(col_name,order_by); $('.active').trigger("click"); }, 5000);
   }
   
-  function callAjax(data){
+  function callAjax(data,pageNo){
     $.ajax({
       type: "GET",
       url: "display.php",
       data: data,        
       dataType: "json",              
       success: function(response){ 
-        $.each( response, function( key, value ) {           
-          item = item + " <tr> <td>" +value.name + "</td> <td>"+value.email +"</td> <td>"+value.message +"</td> <td>"+value.date +"</td> </tr>";               
-        });      
+        $.each( response, function( key, value ) {     
+          result_array[key] = " <tr id='"+ key +"''> <td>" +value.name + "</td> <td>"+value.email +"</td> <td>"+value.message +"</td> <td>"+value.date +"</td> </tr>";               
+        });   
+        var result_item = "";  
+        if(pageNo == 1){
+          for(var k = 0; k < maxRows; k++){
+            result_item = result_item + result_array[k];
+          }
+        }
+        else{
+          var end = pageNo*maxRows;
+          var start = end - maxRows;
+          for(var j = start; j < end; j++){
+            result_item =  result_item + result_array[j];
+          }
+        }
         $(".detail").empty();
-        $(".detail").append(item);
-        item = ""
-        doPagination();
+        $(".detail").append(result_item);
+        result_item = "";
+        doPagination(pageNo);        
       }
     });
   }
+
 });
 
+    
